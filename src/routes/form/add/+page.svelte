aaac4e354bf2c1868abfaa2c398c8e687fa50588
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2023-08-23 11:35:48 -0300

    Firestore added

--- src/routes/form/add/+page.svelte
+++ src/routes/form/add/+page.svelte
@@ -1,7 +1,32 @@
 <script>
-	import { TextArea } from 'carbon-components-svelte';
+	import { FluidForm, TextInput, Button } from 'carbon-components-svelte';
+	import { collection, addDoc } from 'firebase/firestore';
+	import { db } from '$lib/firebase.client';
+	let country = '';
+	let valor = '';
+	$: console.log(country);
+	$: console.log(valor);
+	console.log(db);
+
+	async function sendData() {
+		// the second parameter of the addDoc function is the object to be saved
+		const docRef = await addDoc(collection(db, 'countries'), {
+			country: country,
+			value: valor
+		});
+		console.log('Document written with reference: ', docRef);
+		country = '';
+		valor = '';
+	}
 </script>
 
-<TextArea labelText="App description" placeholder="Enter a description..." />
-
-<h1>Página para adicionar item</h1>
+<FluidForm>
+	<TextInput
+		labelText="País"
+		placeholder="Digite o nome do país..."
+		bind:value={country}
+		required
+	/>
+	<TextInput labelText="População" placeholder="(em milhões)" bind:value={valor} required />
+	<Button on:click={sendData}>Enviar</Button>
+</FluidForm>
--- src/routes/form/delete/+page.svelte
+++ src/routes/form/delete/+page.svelte
@@ -1,1 +1,6 @@
+<script>
+	export let data;
+	console.log(data);
+</script>
+
 <h1>Página para excluir item</h1>
