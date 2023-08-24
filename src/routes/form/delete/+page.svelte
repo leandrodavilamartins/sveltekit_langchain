f0dfa59639264dffcfafe7cb1bb50b7fca8ed51e
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2023-08-23 22:42:47 -0300

    id added to docs object

--- src/routes/form/delete/+page.svelte
+++ src/routes/form/delete/+page.svelte
@@ -1,16 +1,19 @@
 <script>
 	import { db } from '$lib/firebase.client';
 	import { Grid, Row, Column } from 'carbon-components-svelte';
+	import { Button } from 'carbon-components-svelte';
 	import { getDocs, collection } from 'firebase/firestore';
 	import { onMount } from 'svelte';
 
 	let docs = [];
+
 	onMount(async () => {
 		const querySnapshot = await getDocs(collection(db, 'countries'));
-		docs = querySnapshot.docs.map((doc) => doc.data());
+		docs = querySnapshot.docs.map((doc) => {
+			return { id: doc.id, country: doc.data().country, value: doc.data().value };
+		});
 		console.log(docs);
 	});
-	console.log(docs);
 	console.log(db);
 </script>
 
@@ -18,8 +21,10 @@
 	<Grid>
 		{#each docs as doc}
 			<Row>
+				<Column>{doc.id}</Column>
 				<Column>{doc.country}</Column>
 				<Column>{doc.value}</Column>
+				<Column><Button>Excluir item</Button></Column>
 			</Row>
 		{/each}
 	</Grid>
